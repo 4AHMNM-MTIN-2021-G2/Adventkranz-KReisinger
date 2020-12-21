738a35a0702b01ac2144fd4190a55fe17eaf7d1b
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2020-12-21 10:13:14 +0100

    day count implementation

--- Assets/MyGame/Scripts/CountDays.cs
+++ Assets/MyGame/Scripts/CountDays.cs
@@ -1,25 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountDays : MonoBehaviour
 {
+    public Text daysGui;
 
-
-    System.DateTime christmasDay;
+    System.DateTime christmasDay; 
     System.DateTime firstAdvent;
     System.DateTime secondAdvent;
     System.DateTime thirdAdvent;
     System.DateTime fourthAdvent;
+    System.DateTime currentDay = System.DateTime.Now;
+
+    private int days = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         christmasDay = new System.DateTime(2020, 12, 24);
-        firstAdvent = new System.DateTime(2020, 11, 29);
+        firstAdvent = new System.DateTime(2020, 11, 29); 
         secondAdvent = new System.DateTime(2020, 12, 6);
         thirdAdvent = new System.DateTime(2020, 12, 13);
         fourthAdvent = new System.DateTime(2020, 12, 20);
+
+
+        while(currentDay.CompareTo(christmasDay) < 0)
+        {
+            currentDay = currentDay.AddDays(1);
+            days ++;
+        }
+
+        Debug.Log(days);
+        daysGui.text = days.ToString();
+
     }
 
     // Update is called once per frame
